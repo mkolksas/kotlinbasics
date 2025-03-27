7981371dd0383ceac11bcdcd3d46ef80b408065b
eddamax0403 <mwanziaedwin5@gmail.com>
2025-03-27 15:12:40 +0300

    third commit

---
+++ src/inheritance.kt
@@ -0,0 +1,31 @@
+//Parent class/Base class/Super class
+open class Animal{
+    var gender = "Male"
+    var age = 3
+
+    fun makesound(){
+        println("Animal is speaking")
+    }
+}
+
+//Child class/Derived class/Sub class
+class dog:Animal(){
+    fun bark(){
+        println("Woof! Woof!")
+    }
+}
+
+class cat{
+    var color = "White"
+    var hasFurs = true
+
+    fun meow(){
+        println("Meow! Meow!")
+    }
+}
+
+fun main() {
+    var a = Animal()
+    var d = dog()
+    var c = cat()
+}
